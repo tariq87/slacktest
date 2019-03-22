4fbb2753ec206a53dd93e77608380b9f4c09b706
tariqcoupa <tariq.siddiqui@coupa.com>
2019-03-22 15:15:37 +0530

    adding exception handling

--- jumbo.rb
+++ jumbo.rb
@@ -17,65 +17,76 @@ OptionParser.new do |opts|
 
 end.parse!
 
-if File.exist?(options[:host]) && File.exist?(options[:conf])
-	hosts = File.read(options[:host]).split("\n")
-	#puts hosts.class
-	#puts options
-
-	for key,value in YAML.load_file(options[:conf]) do
-		package = ""
-		service = ""
-		filename = ""
-		owner = ""
-		content = ""
-		mode = ""
-		for i, j in value do
+begin
+	
+	if File.exist?(options[:host]) && File.exist?(options[:conf])
+				hosts = File.read(options[:host]).split("\n")
 			
-			if key == "Packages" && i["action"] == "install"
-				package = i["name"]
-				puts package
-				if is_installed?(package, hosts)
-					puts "Package #{package} already installed...Doing nothing"
-				else
-					install_package(package, hosts)
-				end
-			elsif key == "Service" && i["action"] == "start"
-				service = i["name"]
-				#puts service
-				if is_running?(service, hosts)
-					p "Service #{service} already running...doing nothing"
-				else
-					start_service(service, hosts)
-				end 
-# Will override file each time
-			elsif key == "File" && i["action"] == "create"
-				filename = i["name"]
-				#puts filename
-				mode = i["mode"].to_i
-				#puts mode
-				owner = i["owner"]
-				#puts owner
-				content = i["content"]
-				#puts content
-				service = i["notify"]
-				#puts service
-				create_file(filename, hosts, mode, owner, content)
-				if not service.to_s.empty?
-				restart_service(service, hosts)
-				end
 
-			elsif key == "Packages" && i["action"] == "remove"
-				package = i["name"]
-				#puts package
-				if is_installed?(package, hosts)
-					uninstall_package(package, hosts)
-				else
-					puts "Package #{package} not found..."
+				for key,value in YAML.load_file(options[:conf]) do
+					package = ""
+					service = ""
+					filename = ""
+					owner = ""
+					content = ""
+					mode = ""
+					for i, j in value do
+						
+						if key == "Packages" && i["action"] == "install"
+							package = i["name"]
+							puts package
+							if is_installed?(package, hosts)
+								puts "Package #{package} already installed...Doing nothing"
+							else
+								install_package(package, hosts)
+							end
+						elsif key == "Service" && i["action"] == "start"
+							service = i["name"]
+							#puts service
+							if is_running?(service, hosts)
+								p "Service #{service} already running...doing nothing"
+							else
+								start_service(service, hosts)
+							end 
+			# Will override file each time
+						elsif key == "File" && i["action"] == "create"
+							filename = i["name"]
+							#puts filename
+							mode = i["mode"].to_i
+							#puts mode
+							owner = i["owner"]
+							#puts owner
+							content = i["content"]
+							#puts content
+							service = i["notify"]
+							#puts service
+							create_file(filename, hosts, mode, owner, content)
+							if not service.to_s.empty?
+							restart_service(service, hosts)
+							end
+
+						elsif key == "Packages" && i["action"] == "remove"
+							package = i["name"]
+							#puts package
+							if is_installed?(package, hosts)
+								uninstall_package(package, hosts)
+							else
+								puts "Package #{package} not found..."
+							end
+								
+								
+						end 
+					end
 				end
-					
-					
-			end 
-		end
+	else
+			#puts "File not found"
+		raise  "File Not Found"
+
+		
 	end
+		
 end
+
+	
+	
 
